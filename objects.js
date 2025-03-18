059d074ca678793bc4ec8df5e561350962fd7ba3
Vikram-Rai0 <ccodework24@gmail.com>
2025-03-18 22:32:24 +0545

    learn event and object method

--- objects.js
+++ objects.js
@@ -1,24 +1,56 @@
-const person = {
-  name : "vikram",
-  age:23,
-  greet : function(){
-    console.log("hello my name is  " + this.name);
-  }
-}
+// const person = {
+//   name: "vikram",
+//   age: 23,
+//   greet: function () {
+//     console.log("hello my name is  " + this.name);
+//   },
+// };
 
-console.log(person);
-person.greet();
+// console.log(person);
+// person.greet();
 
-const car = {
-  brand: "BMW",
-  weight: "500",
-};
+// const car = {
+//   brand: "BMW",
+//   weight: "500",
+// };
 
-car.color = "red"; // adding new property in object
-car.brand = "tesla"; // updating property
+// car.color = "red"; // adding new property in object
+// car.brand = "tesla"; // updating property
 
-delete car.brand; // deleting property
+// delete car.brand; // deleting property
 
-console.log("added new" + car.color);
-console.log("updated brand name " + car.brand);
-console.log(car);
+// console.log("added new" + car.color);
+// console.log("updated brand name " + car.brand);
+// console.log(car);
+
+// ==========================================================================================
+
+//Nested Object
+
+// const women = {
+//   name: "nabina",
+//   gender: "female",
+//   child: {
+//     name: "mini nabina",
+//     gender: "female",
+
+//     twin: {
+//       name: "mini nabin",
+//       gender: "male",
+//     },
+//   },
+// };
+
+// console.log(women.child);
+// ===============================================================================================
+
+// const country = {
+//   name: "nepal",
+//   capital: "kathmanduu",
+//   welcome: () => {
+//     return "welcome to kathmandu";
+//   },
+// };
+
+// console.log(country);
+// console.log(country.welcome());
