ae1cacefbb24cca1f93b137dedd246be1e53fcc8
Vikram-Rai0 <ccodework24@gmail.com>
2025-03-19 22:17:59 +0545

    learn string and string method in string.js file

---
+++ string.js
@@ -0,0 +1,23 @@
+// let name = "Vikram Rai";
+// console.log(name);
+// const length = name.length;
+// console.log(length);
+
+// string Method
+
+// let introduction = "MY NAME IS is ";
+// let name = "Vikram rai";
+// let xyz = introduction.concat(" ", name);
+// xzy;
+// console.log(xyz);
+// let len = xyz.length();
+
+//String Search
+
+// let str = "lets search the string";
+// let search = str.includes('search');
+// console.log(search)
+
+// String template
+let programming = "java script";
+console.log(`i am a ${programming} devloper`);
